aff369861044ff204dec9ea0fa74ba4d51be0496
artur.yumaev <artur.yumaev@sbermarket.ru>
2024-10-02 19:17:56 +0300

    Added longest-substring-without-repeating-characters

---
+++ medium/longest-substring-without-repeating-characters/main.go
@@ -0,0 +1,47 @@
+package main
+
+func lengthOfLongestSubstring(s string) int {
+	max := nUnique(s)
+
+	for i := len(s) - max; i < len(s); i++ {
+		for j := 0; j < i+1; j++ {
+			subs := s[j : len(s)-i+j]
+
+			if hasNoDuplicates(subs) {
+				return len(subs)
+			}
+		}
+	}
+
+	return 0
+}
+
+func hasNoDuplicates(s string) bool {
+	m := make(map[rune]struct{}, len(s))
+
+	for _, c := range s {
+		if _, exists := m[c]; !exists {
+			m[c] = struct{}{}
+		} else {
+			return false
+		}
+	}
+
+	return true
+}
+
+func nUnique(s string) int {
+	m := make(map[rune]struct{}, len(s))
+
+	for _, c := range s {
+		if _, exists := m[c]; !exists {
+			m[c] = struct{}{}
+		}
+	}
+
+	return len(m)
+}
+
+func main() {
+	_ = lengthOfLongestSubstring("abc")
+}
